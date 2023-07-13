88488be40304cff8a35a6a210cade173a831ffec
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-07-13 22:44:00 +0530

    update Time problem settled

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", function (req, res) {
-   res.sendFile(__dirname+"/index.html");   
+   res.sendFile(path.join(__dirname,"/index.html"));   
 });
 
 app.listen("3000", function () {
